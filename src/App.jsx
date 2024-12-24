c50db4e727ed227033721cd3a4bd1ac2d92f446d
bitblender2 <wkaimah2@gmail.com>
2024-12-24 23:26:00 +0200

    done the boxes challenge

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div className="w-[100%] h-[100vh] flex justify-center items-center bg-zinc-400 text-white">
+      <div className="w-[30%] h-[50vh] bg-purple-900 p-3 flex flex-col justify-center ">
+        <div className="bg-pink-600 w-[100px] h-[100px] flex self-center ml-6"></div>
+        <div className="bg-gray-50 w-[200px] h-[100px] ml-10"></div>
+        <div className="bg-purple-500 w-[250px] h-[100px] flex self-end mr-16"></div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
